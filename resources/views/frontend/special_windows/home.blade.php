e67600f2931b9a9b491ddf6402ed996054e455e8
skuehn22 <kuehn.sebastian@gmail.com>
2018-07-30 18:40:45 +0100

    Mobiledetect (Jenssegers\Agent\Agent)

---
+++ resources/views/frontend/special_windows/home.blade.php
@@ -0,0 +1,21 @@
+@extends('frontend.master')
+@section('seo')
+@stop
+@section('css')
+
+
+@stop
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+               Test for Windows
+            </div>
+        </div>
+    </div>
+
+@stop
+@section("js")
+@stop
