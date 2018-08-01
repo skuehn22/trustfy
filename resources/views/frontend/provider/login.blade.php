dd7c8e99b09848d1a578494cf86b67cd06d4f986
skuehn22 <kuehn.sebastian@gmail.com>
2018-08-01 10:57:06 +0100

    dashboard blade

--- resources/views/frontend/provider/login.blade.php
+++ resources/views/frontend/provider/login.blade.php
@@ -6,7 +6,91 @@
 @stop
 
 @section('content')
-    Panzerschweine rollen!
+
+    <div class="container dashboard">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            <i class="far fa-envelope"></i> Inbox</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-xs-6 col-md-6">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            <i class="fas fa-magic"></i> Quick Shortcuts</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                                <a href="/en/contracts" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                    <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Create Contract</p>
+                                </a>
+                            </div>
+                            <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                                <a href="/en/contracts" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                    <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">View Project</p>
+                                </a>
+                            </div>
+                            <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                                <a href="/en/contracts" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                    <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">View Project</p>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                                <a href="/en/contracts" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                    <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Create Contract</p>
+                                </a>
+                            </div>
+                            <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                                <a href="/en/contracts" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                    <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Create Contract</p>
+                                </a>
+                            </div>
+                            <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                                <a href="/en/contracts" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                    <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Create Contract</p>
+                                </a>
+                            </div>
+                        </div>
+                        <!--<a href="http://www.jquery2dotnet.com/" class="btn btn-success btn-lg btn-block" role="button"><span class="glyphicon glyphicon-globe"></span> Website</a>-->
+                    </div>
+                </div>
+                <!--
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">
+                            <i class="fas fa-chart-line"></i> KPIs</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-xs-12 col-md-12">
+                                <a href="/en/contracts" class="btn btn-danger btn-lg" role="button"><span class="glyphicon glyphicon-list-alt"></span> <br/>Apps</a>
+                                <a href="#" class="btn btn-warning btn-lg" role="button"><span class="glyphicon glyphicon-bookmark"></span> <br/>Bookmarks</a>
+                                <a href="#" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-signal"></span> <br/>Reports</a>
+                                <a href="#" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-comment"></span> <br/>Comments</a>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+        -->
+            </div>
+        </div>
+    </div>
+
 @stop
 @section("js")
 @stop
