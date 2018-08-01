a353dfebe5c0bb6eecd5c038fb1b4b1962e9be8c
skuehn22 <kuehn.sebastian@gmail.com>
2018-08-01 18:59:20 +0100

    Backend Contracts Types

---
+++ resources/views/backend/contracts/types.blade.php
@@ -0,0 +1,44 @@
+<div class="contract-types">
+    <div class="row">
+        <div class="col-md-12">
+            <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                            <a href="#" class="btn btn-outline-grey btn-lg panel-btn" id="contract-types" role="button" data-toggle="modal" data-target="#exampleModalCenter">
+                                <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Create Proposal</p>
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                            <a href="#" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Create Contract</p>
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                            <a href="#" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Create Invoice</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                            <a href="#" class="btn btn-outline-grey btn-lg panel-btn" id="contract-types" role="button" data-toggle="modal" data-target="#exampleModalCenter">
+                                <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Create Reminder</p>
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-md-4 shortcuts-boxes">
+                            <a href="#" class="btn btn-outline-grey btn-lg panel-btn" role="button">
+                                <i class="fas fa-file-alt"></i> <br/><p style="padding-top: 11px;">Blank Document</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
