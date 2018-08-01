ac5c76103db9b3e58f4bc99a7fb8deba7d86040b
skuehn22 <kuehn.sebastian@gmail.com>
2018-08-01 18:21:34 +0100

    routes provider

--- app/Http/Routes/Backend/provider.php
+++ app/Http/Routes/Backend/provider.php
@@ -9,8 +9,15 @@
 
 Route::group(['prefix' => LaravelLocalization::setLocale()], function()
 {
+
+    /* Provider Dashboard */
     Route::any('/provider/dashboard', 'Backend\Provider\DashboardController@index') ;
+
+    /* Provider Contracts */
     Route::any('/provider/contract-types', 'Backend\Provider\ContractsController@getTypes') ;
+
+    /* Provider Settings */
     Route::any('/provider/settings', 'Backend\Provider\SettingsController@index') ;
+    Route::any('/provider/settings/save-company', 'Backend\Provider\SettingsController@saveCompany') ;
 
 });
