90642f309aa97d4e0ebd5d41d491b3e0f269494d
skuehn22 <kuehn.sebastian@gmail.com>
2018-11-19 17:00:39 +0000

    favico n

--- resources/views/frontend/master-trustfy.blade.php
+++ resources/views/frontend/master-trustfy.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link href="{{ asset('css/style-trustfy.css') }}" rel="stylesheet" type="text/css" >
-
+    <link rel="icon" type="image/png" href="/favicon-tf.ico">
     @yield('seo')
     <style>
         @yield('css')
