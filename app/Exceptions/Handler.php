29ce30c72645bc98040d2a98b291d60eccf444a6
skuehn22 <kuehn.sebastian@gmail.com>
2019-02-05 12:42:16 +0000

    Error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,100 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+
+
+
+        if($this->isHttpException($e))
+        {
+            if(env("APP_ENV") == "local") {
+
+                return parent::render($request, $e);
+
+
+            } else {
+                $test = "";
+                switch ($e->getStatusCode()) {
+                    // not found
+                    case 404:
+
+
+                        $str = $request." \n --------------- \n".$e;
+
+                        Mail::raw($str, function ($message) {
+                            $message->from('info@trustfy');
+                            $message->subject("trustfy Error");
+                            $message->to('kuehn.sebastian@gmail.com');
+                        });
+
+
+                        $content="<br><br><br><h2>Ooooops!</h2>";
+                        $content.= Lang::get('frontend.handler_error_occured');
+                        $content.="<br><br>Gerne kannst du deine Anfrage an uns per E-Mail senden und <br>wir schicken dir das passende Angebot zu: ";
+                        $content.="<a style='color:red;' href='mailto:info@herden-tt.com'>info@herden-tt.com</a><br><br><br><br>";
+
+                        return Response::view('errors.404',compact('content'));
+                        break;
+                    // internal error
+                    case '500':
+
+
+
+                        $str = $request." \n --------------- \n".$e;
+
+                        Mail::raw($str, function ($message) {
+                            $message->from('info@trustfy');
+                            $message->subject("trustfy Error");
+                            $message->to('kuehn.sebastian@gmail.com');
+                        });
+
+
+                        $content="<br><br><br><h2>Ooooops!</h2>";
+                        $content.= Lang::get('frontend.handler_error_occured');
+                        $content.="<br><br>Gerne kannst du deine Anfrage an uns per E-Mail senden und <br>wir schicken dir das passende Angebot zu: ";
+                        $content.="<a style='color:red;' href='mailto:info@herden-tt.com'>info@herden-tt.com</a><br><br><br><br>";
+
+
+                        return Response::view('errors.404',compact('content'));
+                        break;
+
+                    default:
+                        $content="<br><br><br><h2>Ooooops!</h2>";
+                        $content.= Lang::get('frontend.handler_error_occured');
+                        $content.="<br><br>Gerne kannst du deine Anfrage an uns per E-Mail senden und <br>wir schicken dir das passende Angebot zu: ";
+                        $content.="<a style='color:red;' href='mailto:info@herden-tt.com'>info@herden-tt.com</a><br><br><br><br>";
+                        return Response::view('errors.404',compact('content'));
+                        break;
+                }
+            }
+        }
+        else
+        {
+            if(env("APP_ENV") == "local") {
+
+                return parent::render($request, $e);
+
+            } else {
+
+                $str = $request." \n --------------- \n".$e;
+
+                Mail::raw($str, function ($message) {
+                    $message->from('info@trustfy.io');
+                    $message->subject("trustfy Error");
+                    $message->to('kuehn.sebastian@gmail.com');
+                });
+
+
+                $cities = Cities::where("hidden", "=", "0")
+                    ->orderBy('name', 'asc')
+                    ->lists("name","id");
+
+                $content="<br><br><br><h2>Ooooops!</h2>";
+                $content.= Lang::get('frontend.handler_error_occured');
+                $content.="<br><br>Gerne kannst du deine Anfrage an uns per E-Mail senden und <br>wir schicken dir das passende Angebot zu: ";
+                $content.="<a style='color:red;' href='mailto:info@herden-tt.com'>info@herden-tt.com</a><br><br><br><br>";
+                return Response::view('errors.404',compact('content', 'cities', 'blade'));
+
+            }
+        }
     }
 }
