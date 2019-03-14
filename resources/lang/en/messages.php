94724a956687caada99111f963a3469153e98267
skuehn22 <kuehn.sebastian@gmail.com>
2019-03-14 18:22:07 +0000

    E-Mail

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,12 +3,12 @@
 return [
 
 
-    'subject_typ_1' => 'Down payment received',
+    'subject_typ_1' => 'Trustfy- Payment Plans- Down payment received',
     'subject_typ_2' => 'KYC Document accepted',
     'subject_typ_3' => 'Welcome to Trustfy',
 
-    'payin_done_subject' => 'Trustfy- Payment Plans Down payment received',
-    'payin_done_2' => 'Trustfy- Payment Received (Client Name) made a payment!',
+    'payin_done_subject' => 'Down payment received',
+    'payin_done_2' => 'Payment Received',
     'show_plan' => 'Show Payment Plan',
 
 ];
--- resources/views/emails/payInSucceeded.blade.php
+++ resources/views/emails/payInSucceeded.blade.php
@@ -1,56 +1,385 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html>
 <head>
-    <title>trustyfy.com</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Trusty Payment Plans</title>
     <style>
+        /* -------------------------------------
+            GLOBAL RESETS
+        ------------------------------------- */
 
-        body{
-            color:#0f0f0f;
+        /*All the styling goes here*/
+
+        img {
+            border: none;
+            -ms-interpolation-mode: bicubic;
+            max-width: 100%;
         }
-
-        a{
-            color: #1e8033;
+        body {
+            background-color: #f6f6f6;
+            font-family: sans-serif;
+            -webkit-font-smoothing: antialiased;
+            font-size: 14px;
+            line-height: 1.4;
+            margin: 0;
+            padding: 0;
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
         }
-
-
-        .terms a{
-            color: #9e9e9e;
+        table {
+            border-collapse: separate;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+            width: 100%; }
+        table td {
+            font-family: sans-serif;
+            font-size: 14px;
+            vertical-align: top;
+        }
+        /* -------------------------------------
+            BODY & CONTAINER
+        ------------------------------------- */
+        .body {
+            background-color: #f6f6f6;
+            width: 100%;
+        }
+        /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
+        .container {
+            display: block;
+            margin: 0 auto !important;
+            /* makes it centered */
+            max-width: 580px;
+            padding: 10px;
+            width: 580px;
+        }
+        /* This should also be a block element, so that it will fill 100% of the .container */
+        .content {
+            box-sizing: border-box;
+            display: block;
+            margin: 0 auto;
+            max-width: 580px;
+            padding: 10px;
+        }
+        /* -------------------------------------
+            HEADER, FOOTER, MAIN
+        ------------------------------------- */
+        .main {
+            background: #ffffff;
+            border-radius: 3px;
+            width: 100%;
+        }
+        .wrapper {
+            box-sizing: border-box;
+            padding: 20px;
+        }
+        .content-block {
+            padding-bottom: 10px;
+            padding-top: 10px;
+        }
+        .footer {
+            clear: both;
+            margin-top: 10px;
+            text-align: center;
+            width: 100%;
+        }
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+            color: #999999;
+            font-size: 12px;
+            text-align: center;
+        }
+        /* -------------------------------------
+            TYPOGRAPHY
+        ------------------------------------- */
+        h1,
+        h2,
+        h3,
+        h4 {
+            color: #000000;
+            font-family: sans-serif;
+            font-weight: 400;
+            line-height: 1.4;
+            margin: 0;
+            margin-bottom: 30px;
+        }
+        h1 {
+            font-size: 35px;
+            font-weight: 300;
+            text-align: center;
+            text-transform: capitalize;
+        }
+        p,
+        ul,
+        ol {
+            font-family: sans-serif;
+            font-size: 14px;
+            font-weight: normal;
+            margin: 0;
+            margin-bottom: 15px;
+        }
+        p li,
+        ul li,
+        ol li {
+            list-style-position: inside;
+            margin-left: 5px;
+        }
+        a {
+            color: #1d3e66;
+            text-decoration: underline;
+        }
+        /* -------------------------------------
+            BUTTONS
+        ------------------------------------- */
+        .btn {
+            box-sizing: border-box;
+            width: 100%; }
+        .btn > tbody > tr > td {
+            padding-bottom: 15px; }
+        .btn table {
+            width: auto;
+        }
+        .btn table td {
+            background-color: #ffffff;
+            border-radius: 5px;
+            text-align: center;
+        }
+        .btn a {
+            background-color: #ffffff;
+            border: solid 1px @if(isset($company->color) ) {{ $company->color }} @else #28a745 @endif;
+            border-radius: 5px;
+            box-sizing: border-box;
+            color: @if(isset($company->color) ) {{ $company->color }} @else #28a745 @endif;
+            cursor: pointer;
+            display: inline-block;
+            font-size: 14px;
+            font-weight: bold;
+            margin: 0;
+            padding: 12px 25px;
+            text-decoration: none;
+            text-transform: capitalize;
+        }
+        .btn-primary table td {
+            background-color: @if(isset($company->color) ) {{ $company->color }} @else #28a745 @endif;
+        }
+        .btn-primary a {
+            background-color: @if(isset($company->color) ) {{ $company->color }} @else #28a745 @endif;
+            border-color: @if(isset($company->color) ) {{ $company->color }} @else #28a745 @endif;
+            color: #ffffff;
+        }
+        /* -------------------------------------
+            OTHER STYLES THAT MIGHT BE USEFUL
+        ------------------------------------- */
+        .last {
+            margin-bottom: 0;
+        }
+        .first {
+            margin-top: 0;
+        }
+        .align-center {
+            text-align: center;
+        }
+        .align-right {
+            text-align: right;
+        }
+        .align-left {
+            text-align: left;
+        }
+        .clear {
+            clear: both;
+        }
+        .mt0 {
+            margin-top: 0;
+        }
+        .mb0 {
+            margin-bottom: 0;
+        }
+        .preheader {
+            color: transparent;
+            display: none;
+            height: 0;
+            max-height: 0;
+            max-width: 0;
+            opacity: 0;
+            overflow: hidden;
+            mso-hide: all;
+            visibility: hidden;
+            width: 0;
+        }
+        .powered-by a {
             text-decoration: none;
         }
+        hr {
+            border: 0;
+            border-bottom: 1px solid #f6f6f6;
+            margin: 20px 0;
+        }
+
+        .header-text{
+            font-size: 20px;
+        }
 
 
+        /* -------------------------------------
+            RESPONSIVE AND MOBILE FRIENDLY STYLES
+        ------------------------------------- */
+        @media only screen and (max-width: 620px) {
+            table[class=body] h1 {
+                font-size: 28px !important;
+                margin-bottom: 10px !important;
+            }
+            table[class=body] p,
+            table[class=body] ul,
+            table[class=body] ol,
+            table[class=body] td,
+            table[class=body] span,
+            table[class=body] a {
+                font-size: 16px !important;
+            }
+            table[class=body] .wrapper,
+            table[class=body] .article {
+                padding: 10px !important;
+            }
+            table[class=body] .content {
+                padding: 0 !important;
+            }
+            table[class=body] .container {
+                padding: 0 !important;
+                width: 100% !important;
+            }
+            table[class=body] .main {
+                border-left-width: 0 !important;
+                border-radius: 0 !important;
+                border-right-width: 0 !important;
+            }
+            table[class=body] .btn table {
+                width: 100% !important;
+            }
+            table[class=body] .btn a {
+                width: 100% !important;
+            }
+            table[class=body] .img-responsive {
+                height: auto !important;
+                max-width: 100% !important;
+                width: auto !important;
+            }
+        }
+        /* -------------------------------------
+            PRESERVE THESE STYLES IN THE HEAD
+        ------------------------------------- */
+        @media all {
+            .ExternalClass {
+                width: 100%;
+            }
+            .ExternalClass,
+            .ExternalClass p,
+            .ExternalClass span,
+            .ExternalClass font,
+            .ExternalClass td,
+            .ExternalClass div {
+                line-height: 100%;
+            }
+            .apple-link a {
+                color: inherit !important;
+                font-family: inherit !important;
+                font-size: inherit !important;
+                font-weight: inherit !important;
+                line-height: inherit !important;
+                text-decoration: none !important;
+            }
+            .btn-primary table td:hover {
+                background-color: #34495e !important;
+            }
+            .btn-primary a:hover {
+                background-color: #34495e !important;
+                border-color: #34495e !important;
+            }
+        }
     </style>
 </head>
-
-<body style="color: #0f0f0f; text-align: center;  margin:0; padding:0; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;" bgcolor="#fff">
-<br><br>
-<table align='center' style="color: #0f0f0f; padding:25px; min-width:320px; border-radius: 5px; border: 2px solid #d4d4d4;" width="45%" cellspacing="0" cellpadding="0" bgcolor="#fff">
+<body class="">
+<span class="preheader">This is preheader text. Some clients will show this text as a preview.</span>
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
     <tr>
-        <td style="text-align: center;">
-            <br><br>
-            <a href="http://www.trustfy.io" style="text-decoration:none;" target="_blank">
-                <span style="color:#1e8033; font-size:36px; font-weight: 800; ">trustfy.io</span>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td style="text-align: left;">
+        <td>&nbsp;</td>
+        <td class="container">
+            <div class="content">
 
-            {!! $data['content'] !!}
+                <!-- START CENTERED WHITE CONTAINER -->
+                <table role="presentation" class="main">
 
+                    <!-- START MAIN CONTENT AREA -->
+                    <tr>
+                        <td class="wrapper">
+                            <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+
+                                <tr>
+                                    <td style="text-align: center; padding-bottom: 35px;">
+                                        <img src="http://dev.work-smarter.io/img/trustfy-green.png" width="200px;">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center; padding-bottom: 35px;">
+                                        <p>Great news! {{$client->firstname}} {{$client->lastname}} has made a payment for the following project:</p>
+                                        <p>Project: {{$plan->name}}</p>
+                                        <p>Milestone: {{$milestone->name}}</p>
+                                        <p>Amount: € {{number_format($milestone->amount, 2, ',', ' ')}}</p>
+                                        <p>Your money is being held in a secure account, <br>so go ahead and work with confidence- your money is there for you! </p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center">
+                                        <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                                            <tbody>
+                                            <tr>
+                                                <td align="center">
+                                                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td  style="text-align: center">  <a href="{{ $planUrl }}" target="_blank">View Plan</a> </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+
+
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <!-- END MAIN CONTENT AREA -->
+                </table>
+                <!-- END CENTERED WHITE CONTAINER -->
+
+                <!-- START FOOTER -->
+                <div class="footer">
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td class="content-block">
+                                <span class="apple-link">Powered by Trustfy</span>
+                                <br> The secure escrow payment system for freelancers and their clients. <a href="https://www.trusty.io">Get started</a>.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="content-block powered-by">
+                                © <a href="https://www.trusty.io">trustfy.io</a> 2019. All Rights Reserved.
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <!-- END FOOTER -->
+
+            </div>
         </td>
-    </tr>
-    <tr>
-        <td style="text-align: center;">
-            <br><br>
-            <span style="color: #9e9e9e;font-family: Arial, Helvetica, sans-serif;font-size: 14px;">
-            <p>&copy; <a href="http://www.trustfy.io" style="color: #1e8033; text-decoration: none; ">trustfy.io</a> 2019. All Rights Reserved. | <span class="terms"><a href="https://www.trustfy.io/en/terms" target="_blank">Terms and Conditions</a></span><br> <br></p>
-            </span>
-        </td>
+        <td>&nbsp;</td>
     </tr>
 </table>
-
 </body>
 </html>
