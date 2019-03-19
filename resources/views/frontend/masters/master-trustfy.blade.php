6afd88fd77275c4bb46815b40e98252c4a37a8a2
skuehn22 <kuehn.sebastian@gmail.com>
2019-03-19 13:52:13 +0000

    Link

--- resources/views/frontend/masters/master-trustfy.blade.php
+++ resources/views/frontend/masters/master-trustfy.blade.php
@@ -79,7 +79,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <a class="navbar-brand js-scroll-trigger" href="/">
             <img src="{{ asset('img/trustfy-new-mixed.png') }}" style="max-width: 200px;" class="img-fluid logo-desktop" alt="Trustfy Freelancer Payment">
         </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
