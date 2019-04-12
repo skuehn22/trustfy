2bc1550b309635dc4a49671a6f926fece0a68fd0
skuehn22 <kuehn.sebastian@gmail.com>
2019-04-12 15:21:55 +0100

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,6 @@
         <div class="col-md-12">
             <div class="panel panel-default" style="padding:20px;">
                 <div class="panel-heading"><h3>Register</h3></div>
-                <p style="font-size: 14px;">Register below and be the first to test and have your voice heard!</p>
                 <p><hr></p>
                 @if(Session::has('error'))
                     <div class="alert alert-danger error_message">
