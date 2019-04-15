bd915a31ec6d190aa9903769ff7aaea4da735f3c
skuehn22 <kuehn.sebastian@gmail.com>
2019-04-15 09:51:24 +0100

    Tracking

--- resources/views/frontend/masters/sign.blade.php
+++ resources/views/frontend/masters/sign.blade.php
@@ -11,25 +11,11 @@
 
 @yield('css')
 
-@include('frontend.masters.elements.tracking')
+@if (env('APP_ENV')=='live')
+    @include('frontend.masters.elements.tracking')
+@endif
 
-<!-- Facebook Pixel Code -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '431587014259346');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=431587014259346&ev=PageView&noscript=1"
-        /></noscript>
-    <!-- End Facebook Pixel Code -->
+
 </head>
 
 <body style="background-color: #dadada">
