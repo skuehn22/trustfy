f2864a297f442216bdf0aebc589fefc3901dc650
skuehn22 <kuehn.sebastian@gmail.com>
2019-04-18 11:32:30 +0100

    Translation

--- resources/lang/de/index.php
+++ resources/lang/de/index.php
@@ -15,5 +15,58 @@ return [
 
     'failed' => 'DEUTSCH',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'project' => 'Projekt',
+
+    'home' => 'home',
+    'about_us' => 'Über Uns',
+    'contact' => 'Kontakt',
+    'sign_in' => 'Anmelden',
+
+
+    'heading_1' => 'Zahlen Ihre Kunden spät?',
+    'heading_2' => 'Musst du immer deinem Geld nachrennen?',
+    'heading_3' => 'Wir sorgen dafür, dass du pünktlich bezahlt wirst.',
+    'heading_4' => 'Zahlungssicherheit für Freelancer und deren Kunden',
+    'sign_up' => 'Kostenlos anmelden',
+
+    'escrow' => 'An easy to use escrow payment system <br>that builds trust between you and your client.',
+    'escrow_sub' => 'Your client makes a payment into a secure Trustfy account and <br> you can work with confidence knowing your money is there.',
+
+    'how' => 'Wie es funktioniert',
+    'how1' => '<br>Select how an when you\'d like to <br>get paid and share it with your client.',
+    'how2' => '<br>Your client receives the request <br>and makes a payment into a secure account.',
+    'how3' => '<br>The money is held securely until a project  <br>or milestone iscomplete - then your payment is released!',
+
+    'advantages' => 'Vorteile',
+    'advantage1' => 'No more chasing payment',
+    'advantage2' => 'Get paid much faster',
+    'advantage3' => 'Confidence in cash flow',
+    'advantage4' => 'Protection from unresponsive clients',
+
+    'fee' => 'All for a fair fee of 3%',
+    'fee1' => 'You only pay when you get paid.',
+    'fee2' => 'Advantages',
+
+    'get_started' => 'GET STARTED',
+
+    'news' => 'Sign up for the news!',
+    'your_mail' => 'Your EMAIL',
+    'sign_up2' => 'Sign up',
+
+    'contact_form' => 'Get in Contact',
+    'contact1' => 'Give us a shout! We\'d love to hear from you.',
+    'contactname' => 'Name',
+    'contactmail' => 'E-Mail',
+    'contactmsg' => 'Message',
+
+    'send' => 'send',
+
+    'claim' => 'We build trust between<br> freelancers and their clients',
+    'terms' => 'Terms',
+    'privacy' => 'Privacy',
+
+
+
+
 
 ];
--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -6,10 +6,144 @@ return [
     'project' => 'Project',
     'amount' => 'Amount',
     'milestone' => 'Milestone',
-
     'greetings' => 'Kind Regards',
-
-
     'no_messages' => 'No messages so far',
+
+    'home' => 'home',
+    'about_us' => 'about us',
+    'contact' => 'contact',
+    'sign_in' => 'sign in',
+
+    'escrow' => 'An easy to use escrow payment system <br>that builds trust between you and your client.',
+    'escrow_sub' => 'Your client makes a payment into a secure Trustfy account and <br> you can work with confidence knowing your money is there.',
+
+    'heading_1' => 'GET PAID ON TIME',
+    'heading_2' => 'Are you regularly chasing payments?',
+    'heading_3' => 'We get you paid on time.',
+    'heading_4' => 'Payment protection for freelance work',
+    'sign_up' => 'Sign up for free',
+
+    'how' => 'How it works',
+    'how1' => '<br>Select how an when you\'d like to <br>get paid and share it with your client.',
+    'how2' => '<br>Your client receives the request <br>and makes a payment into a secure account.',
+    'how3' => '<br>The money is held securely until a project  <br>or milestone iscomplete - then your payment is released!',
+
+    'advantages' => 'Advantages',
+    'advantage1' => 'No more chasing payment',
+    'advantage2' => 'Get paid much faster',
+    'advantage3' => 'Confidence in cash flow',
+    'advantage4' => 'Protection from unresponsive clients',
+
+    'fee' => 'All for a fair fee of 3%',
+    'fee1' => 'You only pay when you get paid.',
+    'fee2' => 'Advantages',
+
+    'get_started' => 'GET STARTED',
+
+    'news' => 'Sign up for the news!',
+    'your_mail' => 'Your EMAIL',
+    'sign_up2' => 'Sign up',
+
+    'contact_form' => 'Get in Contact',
+    'contact1' => 'Give us a shout! We\'d love to hear from you.',
+    'contactname' => 'Name',
+    'contactmail' => 'E-Mail',
+    'contactmsg' => 'Message',
+
+    'send' => 'send',
+
+    'claim' => 'We build trust between<br> freelancers and their clients',
+    'terms' => 'Terms',
+    'privacy' => 'Privacy',
+
+    'about_us2' => 'About Us',
+    'about1' => '"My dad was a contractor, so I grew up watching him struggle to get paid. Chasing a payment after he\'d done good work really chipped away at his pride.',
+    'about2' => 'When I started freelancing myself, I got to feel that first hand.',
+    'about3' => 'I want to change the way things are done because no one should have to worry if their money is safe. Not freelancers. Not clients."',
+    'about4' => 'Co-founder and CTO',
+    'about5' => 'We founded Trustfy because we want to make the future of work fairer, more transparent and more sustainable for everyone involved. It’s why we get up in the morning.',
+    'about6' => 'More and more people are working for themselves and that has the potential to be a great thing.',
+    'about7' => 'But it also means clients don’t have the security of working with a larger firm and freelancers don’t have the security of a full-time employer.',
+    'about8' => 'When it comes to paying and getting paid, that can cause issues for everyone involved. We, and a lot of people we care dearly about, have lost the better part of our sanity in this process.',
+    'about9' => 'As a client, you’re asked to pay a good chunk of the bill up front- then you cross your fingers that everything works out.',
+    'about10' => 'As a freelancer, you do the work and cross your fingers that you won’t be chasing payments later.',
+    'about11' => 'It’s stressful for everyone involved. And it’s unnecessary. That\'s why we\'re making the system fairer, so that everyone involved can have the peace of mind they deserve.',
+
+    'faq' => 'FAQ - General',
+    'faq1' => '<p>&nbsp;</p>
+                <h3>What is escrow?</h3>
+                <p>Escrow might sound complicated, but it&rsquo;s actually a really simple process!</p>
+                <p>It means that during a financial transaction, a trusted third party holds onto the money and releases it at the end.</p>
+                <p>Here&rsquo;s an example: Imagine you&rsquo;re selling a watch online. You wouldn&rsquo;t want to ship it to a buyer unless they&rsquo;ve paid you, right? But the buyer doesn&rsquo;t want to transfer you the money until they have the watch. This is where escrow comes into play: A trusted third person or company, like Ebay, holds on to that money until the watch has been delivered. Then they release it to you.</p>
+                <p>&nbsp;</p>
+                <h3>So Trustfy is an escrow system for freelancers, what exactly does that mean?</h3>
+                <p>It means you can pay for a freelance project with peace of mind, knowing that your money will only be released when you&rsquo;re satisfied.</p>
+                <p>At the start of a project, your freelancer will send you a payment plan for the project- from there you can easily pay their fee into our secure escrow account, it just takes a minute. We&rsquo;ll hold on to that money until your freelancer completes a project milestone or the complete project, whatever you&rsquo;ve agreed to.</p>
+                <p>Once they&rsquo;ve marked their work as complete, you&rsquo;ll receive a notification and can release their fee at the touch of a button. We hate bureaucracy as much as you do, so we&rsquo;re keeping this process as simple as possible!&nbsp;</p>
+                <p>&nbsp;</p>
+                <h1>FAQ - For Clients</h1>
+                <p>&nbsp;</p>
+                <h3>When should I use Trustfy?</h3>
+                <p>Trustfy is the most secure way of paying your freelancer, so it&rsquo;s ideal for most transactions. Now, if you&rsquo;ve been working with your freelancer for years, or she&rsquo;s your best friend&rsquo;s sister, we get it. You probably don&rsquo;t need the extra security.</p>
+                <p>If you&rsquo;re not 100% comfortable paying your freelancer up front, then don&rsquo;t! It takes you 2 minutes to pay into our secure account and 1 minute to release those funds when you&rsquo;re happy. It doesn&rsquo;t get any easier than that.</p>
+                <p>&nbsp;</p>
+                <h3>So that means my freelancer can&rsquo;t access the money I&rsquo;ve paid into Trustfy?</h3>
+                <p>Exactly, your freelancer can&rsquo;t touch that money until you release it. We securely hold your money until your freelancer has finished the project or a project milestone- whatever you have agreed to. Once you’re satisfied with their work, you simply click "release payment" and the money is transferred directly to your freelancer.</p>
+                <p>You don&rsquo;t have to pay up front for work you haven&rsquo;t seen and your freelancer can feel great about starting work with you.</p>
+                <p>&nbsp;</p>
+                <h3>How can I pay into Trustfy&rsquo;s secure account?</h3>
+                <p>We accept Master and Visa card payments as well as bank transfers.</p>
+                <p>&nbsp;</p>
+                <h3>I&rsquo;m not happy with the quality of work my freelancer delivered, what do I do?</h3>
+                <p>First things first, talk to your freelancer! Chances are, they want this project to go as well as you do. You have 2 weeks from the day your freelancer marks a milestone/project as complete before the money is released, so use that time to communicate and see what your freelancer can do to fix the problem.</p>
+                <p>If you&rsquo;re still not satisfied or your freelancer hasn&rsquo;t responded to calls or messages in a couple of days, you can open a dispute. That will freeze any funds and we&rsquo;ll reach out to you and your freelancer to see what&rsquo;s going on.</p>
+                <p>&nbsp;</p>
+                <h3>So I&rsquo;ve opened a dispute, what happens now?</h3>
+                <p>The first thing we do when you open a dispute is freeze all the money related to your project. So don&rsquo;t worry, it&rsquo;s safe!</p>
+                <p>Then our customer service team will get in touch with you and your freelancer to talk about what&rsquo;s going on. We&rsquo;ll take a look at what you&rsquo;ve agreed to in your contract or project plan and help you find a solution.</p>
+                <p>If your freelancer is a no-show and doesn&rsquo;t respond, we&rsquo;ll release your money back to you.</p>
+                <p>&nbsp;</p>
+                <h3>What happens if I cancel the contract?</h3>
+                <p>That depends on the contract or terms of service you&rsquo;ve agreed to with your freelancer. If there&rsquo;s a termination clause of some sort, we respect your agreement and release the money accordingly.</p>
+                <p>&nbsp;</p>
+                <p>If you&rsquo;re not sure about what your contract or agreement says, check your inbox or log in to your Trustfy account. It was attached to the payment plan you received from your freelancer at the start of the project and we&rsquo;ve got it saved for you. Just register using the email that your freelancer is using to communicate with you and it will be right there anytime you need it.</p>
+                <p>&nbsp;</p>
+                <h1>FAQ - For Freelancers</h1>
+                <p>&nbsp;</p>
+                <h3>So that means my client can&rsquo;t access the money paid into Trustfy during the project?</h3>
+                <p>Exactly, your client can&rsquo;t touch that money while you&rsquo;re working on the project.</p>
+                <p>We securely hold your client&rsquo;s money until you&rsquo;ve completed your side of the bargain. Once you&rsquo;ve finished a project or a project milestone, you simply mark it as complete in your Trustfy account. Your client receives a notification and can release your payment at the touch of a button.</p>
+                <p>You don&rsquo;t have to worry about chasing invoices and your client can feel confident working with you.</p>
+                <h3>&nbsp;</h3>
+                <h3>When should I use Trustfy?</h3>
+                <p>Trustfy is the most secure way of getting paid if you&rsquo;re self-employed or freelancing. We make sure you can focus on your work without having to worry about getting paid at the end. It&rsquo;s ideal for most transactions, but if you&rsquo;ve known your client for years or she&rsquo;s your best friend&rsquo;s sister, we get it. You probably don&rsquo;t need the extra security.</p>
+                <p>If you haven&rsquo;t worked with a client before or they&rsquo;ve got a reputation for paying late, escrow is a great solution. It&rsquo;s especially useful if your client doesn&rsquo;t have the same postcode as you do. Chasing invoices is bad, but chasing invoices from afar is even worse.</p>
+                <p>If you&rsquo;re uncomfortable asking your client for a large up front deposit,or your client isn&rsquo;t willing to pay one, Trustfy can be a great alternative.</p>
+                <p>And the process is really easy on your client. It takes 2 minutes to pay into our secure account and 1 minute to release those funds when you&rsquo;ve completed the work. It&rsquo;s no more complicated than paying an ordinary invoice.</p>
+                <h3>&nbsp;</h3>
+                <h3>What fees do you take?</h3>
+                <p>We’re dedicated to keeping our prices simple and fair, we just take a 3% transaction fee.</p>
+                <p>That&rsquo;s it. No extra fees. No cash-out expenses. No small print. And no surprise fees on holidays and days that end in y.</p>
+                <p>&nbsp;</p>
+                <h3>Why should I upload a contract?</h3>
+                <p>Having a written record of what you&rsquo;ve promised your client is important. It prevents miscommunication and it&rsquo;s an important point of reference as the project progresses. How many edits are included in the contract? What happens if something comes up and the client has to cancel? Who owns the IP?</p>
+                <p>We hope it never happens, but If you or your client ever open a dispute, a written contract makes all the difference. If our customer service team takes a look at a dispute and there&rsquo;s no record of what you&rsquo;ve agreed to, your client can have a good case for not releasing a payment.</p>
+                <p>A contract doesn&rsquo;t have to be fancy. Get your terms of service down on paper. Upload your project plan with detailed deliverables. Write down what you expect of your client and what they can expect of you.</p>
+                <p>You don&rsquo;t need a lawyer drafting up expensive legal documents that the average person can&rsquo;t understand. You do need to get your agreement with your client down on paper and send it over with your payment plan.</p>
+                <p>&nbsp;</p>
+                <h3>What happens if my client cancels the contract?</h3>
+                <p>What does your contract say? Or your project plan? Terms of service?</p>
+                <p>Get a written agreement in place with your client, otherwise they can just cancel a project out of the blue.</p>
+                <p>Whatever documents you sent your client alongside the payment plan are the point of reference. They are meant to protect you and your client so that you both get a fair deal.&nbsp;&nbsp;&nbsp;</p>
+                <p>&nbsp;</p>
+                <h3>My client is refusing to release a payment, what do I do?</h3>
+                <p>First things first, talk to them! What are they unhappy about? Are you both sticking to the terms of your contract? Chances are, your client would prefer a friendly conversation with you than an email from us asking what&rsquo;s going on.</p>
+                <p>If your client isn&rsquo;t responding to you and you&rsquo;ve marked a milestone as complete, don&rsquo;t panic. Unless your client opens a dispute with us, that money will be automatically released to you at the 2 week mark.</p>',
+
+
+
+
+
+
 
 ];
