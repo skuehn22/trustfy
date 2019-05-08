c6a1022c9d5cf6f214e62d5af907c3a59414cfb5
skuehn22 <kuehn.sebastian@gmail.com>
2019-05-08 13:30:17 +0100

    update

--- resources/views/frontend/blog_post1.blade.php
+++ resources/views/frontend/blog_post1.blade.php
@@ -2,9 +2,8 @@
 
 @section('seo')
 
-    <title>{!! trans('seo.title_escrow') !!}</title>
-    <meta name="description" content="{!! trans('seo.desc_escrow') !!}">
-    <meta name="keywords" content="{!! trans('seo.keywords_escrow') !!}">
+    <title>Freelance  Economy</title>
+    <meta name="description" content="Stop calling it the gig economy, it’s the future of work.">
 
 @endsection
 
@@ -47,36 +46,25 @@
                     <br><br>
                     <img src="/img/gig.jpg" style="width: 100%" class="img-responsive" alt="Freelancer not gig economy"> Photo by: Wenni Zhou
                     <br><br>
-                    <p>We’re not a huge fan of the term “Gig Economy.” Firstly, the Gig Economy makes freelancing sound niche, somehow separate from the larger economy. Freelancers turned over 121 billion dollars in the UK and 1.4 trillion in the US last year- those numbers don’t qualify as niche in our book.  36% of the US workforce is already freelancing today, and that number will be over 50% by 2027. <strong>That means that every second person will be working for themselves within the next 10 years. </strong></p>
+                    <p class="text-justify">We’re not a huge fan of the term “Gig Economy.” Firstly, the Gig Economy makes freelancing sound niche, somehow separate from the larger economy. Freelancers turned over 121 billion dollars in the UK and 1.4 trillion in the US last year- those numbers don’t qualify as niche in our book.  36% of the US workforce is already freelancing today, and that number will be over 50% by 2027. <strong>That means that every second person will be working for themselves within the next 10 years. </strong></p>
 
-                    <p>
+                    <p class="text-justify">
                         More importantly, “Gig Economy” reinforces negative stereotypes around freelancing and gives the impression freelancers shouldn’t be taken seriously or treated professionally. It’s not work, it’s a gig. As part of this, we need to do a better job of differentiating between abusive employment practices and freelance work.  <strong>Warehouse workers or drivers being forced to take “independent contractor” status that offers no employment benefits is not the same thing as self-employed people using their unique skill set to work for themselves.</strong>
                     </p>
-                    <p>
+                    <p class="text-justify">
                         For many freelancers, self-employment has been an overwhelmingly positive and empowering experience. IPSE, the voice of the UK’s self-employed population, recently conducted a survey on the psychological dimensions of freelancing. They found a positive link between freelancing, job satisfaction and overall well-being. 84% of freelancers are highly satisfied with their work and an incredible 95% have a sense of pride in the work they do. Most striking is that <strong>only 3% of those surveyed would make the switch back to employee.</strong>
                     </p>
-                    <p>
+                    <p class="text-justify">
                         To be sure, we’re not here to tell you that self-employment has no downsides. There certainly are. Any freelancer will tell you how hard it is to run your own business: you’re marketing, sales, product, accounting and administration all rolled into one. Often in one day.
                     </p>
-                    <p>
+                    <p class="text-justify">
                         And clients aren’t always making life easy. Over 70% of freelancers are being paid late and in certain industries like fashion and construction, that number is over 80%. <strong>It would be unimaginable if 80% of employees would struggle to collect their paychecks. Freelancers deserve that same security.</strong>
                     </p>
-                    <p>
+                    <p class="text-justify">
                         We need to acknowledge these radical shifts and do a better job of supporting freelancers.
                     </p>
-                    <p>
+                    <p class="text-justify">
                         <strong>Freelance work is work, not a gig, and it deserves to be fully valued!</strong>
-                    </p>
-
-                    <p>
-                        <i>
-                        Sources<br>
-                        Check out IPSE’s study on freelancer job satisfaction and well-being:<br>
-                        https://www.ipse.co.uk/resource/job-satisfaction-and-wellbeing.html<br>
-
-                        and the Freelancers Union report on the costs of nonpayment:<br>
-                        https://www.freelancersunion.org/resources/r/report-the-costs-of-nonpayment<br>
-                        </i>
                     </p>
                     <br><br><br> <br><br><br>
                 </div>
